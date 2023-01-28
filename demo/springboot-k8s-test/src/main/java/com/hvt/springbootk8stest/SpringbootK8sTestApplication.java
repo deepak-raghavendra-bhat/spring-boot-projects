4cfec4d318b7e742d4fa187e2b192ecf9a5ce988
deepak-raghavendra-bhat <deepak.raghavendra.bhat@outlook.com>
2023-01-29 05:10:34 +0530

    Added demo project for starter pack
    Added demo project for starter pack

---
+++ demo/springboot-k8s-test/src/main/java/com/hvt/springbootk8stest/SpringbootK8sTestApplication.java
@@ -0,0 +1,14 @@
+package com.hvt.springbootk8stest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SpringbootK8sTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sTestApplication.class, args);
+	}
+
+}
---
+++ demo/springboot-k8s-test/src/test/java/com/hvt/springbootk8stest/SpringbootK8sTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvt.springbootk8stest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
